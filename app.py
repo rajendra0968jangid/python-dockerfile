d0af819ceabd8f5e9c4454f3d54d2e2a889f7f08
Rajendra jangid <120442903+rajendra0968jangid@users.noreply.github.com>
2025-07-08 15:57:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome devops developers"
+    return "Welcome devops developers & engineer"
 
 @app.route("/how-are-you")
 def hello():
